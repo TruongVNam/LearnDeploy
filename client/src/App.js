f5bc196d549a7bfea28561cb393b4540767bc93e
TruongVNam <truongnam.css.yds@gmail.com>
2023-03-29 22:00:10 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
-import {createBrowserRouter, RouterProvider, Router } from "react-router-dom";
+import {createBrowserRouter, RouterProvider, Router, createHashRouter } from "react-router-dom";
 import Users from './components/Users';
 import UserDetail from './components/UserDetail';
 import CreateUser from './components/CreateUser';
 
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   { path:"/", element: <Users/>},
   { path:"/createuser", element: <CreateUser/>},
   // { 
